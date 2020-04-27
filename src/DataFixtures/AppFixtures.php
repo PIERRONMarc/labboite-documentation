9c7cd2d2fe1f49b27d65d076ea71cf091ed06a19
PIERRON Marc <pierronmarc@outlook.fr>
2020-04-27 15:25:24 +0200

    feat: fixtures (themes/categories/tools)

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Tool;
+use App\Entity\Theme;
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = \Faker\Factory::create('fr_FR');
+
+
+        $themesName = ['Outils et machines', 'Logiciels'];
+        $themes = array();
+        foreach ($themesName as $t) {
+            $theme = new Theme();
+            $theme->setName($t);
+            $manager->persist($theme);
+            array_push($themes, $theme);
+        }
+
+        $categories = array();
+        foreach ($themes as $theme) {
+            for ($i=0; $i < 5; $i++) { 
+                $category = new Category();
+                $category->setName($faker->word())
+                    ->setTheme($theme)
+                    ->setDisplayOrder($i)
+                ;
+                $manager->persist($category);
+                array_push($categories, $category);
+            }
+        }
+
+        foreach ($categories as $category) {
+            for ($i=0; $i < 5; $i++) { 
+                $tool = new Tool();
+                $tool->setName($faker->word())
+                    ->setPicturePath('https://via.placeholder.com/150')
+                    ->setDescription($faker->text())
+                    ->setType($faker->word())
+                    ->setDisplayOrder($i)
+                    ->setCategory($category)
+                ;
+                $manager->persist($tool);
+            }
+        }
+
+        $manager->flush();
+    }
+}
