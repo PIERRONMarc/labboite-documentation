1fdcff3f4c900c386f02fdcd84b97b0b46e1cb3d
PIERRON Marc <pierronmarc@outlook.fr>
2020-04-27 20:47:18 +0200

    feat: CRUD tool

---
+++ src/Form/ToolType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Tool;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ToolType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('picturePath')
+            ->add('description')
+            ->add('type')
+            ->add('displayOrder')
+            ->add('category', EntityType::class, [
+                'class' => Category::class, 
+                'choice_label' => 'name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Tool::class,
+        ]);
+    }
+}
