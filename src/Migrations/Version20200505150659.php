027f1e3bd2b72bd6957492e93b676c41c61502fa
PIERRON Marc <pierronmarc@outlook.fr>
2020-05-05 17:15:45 +0200

    fix: reset migration files

--- src/Migrations/Version20200505150659.php
+++ src/Migrations/Version20200505150659.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200505134109 extends AbstractMigration
+final class Version20200505150659 extends AbstractMigration
 {
     public function getDescription() : string
     {
